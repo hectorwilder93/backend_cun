b6ea0024c29f46c36ffde9eef634b982a892a731
DAR <DAR21032024@gmail.com>
2026-08-26 21:34:15 -0500

    actualizado

---
+++ servicio/JARiveros.js
@@ -0,0 +1,23 @@
+const array = [
+  { name: "monteria" },
+  { name: "caucasia" },
+  { name: "tierralta" },
+  { name: "planeta rica" },
+  { name: "sahagun" },
+  { name: "cerete" },
+  { name: "santa cruz de lorica" },
+  { name: "puerto escondido" },
+  { name: "montelibano" },
+  { name: "ayapel" },
+  { name: "san jose de uré" },
+  { name: "san andres de sotavento" },
+  { name: "san carlos" },
+  { name: "san pelayo" },
+  { name: "tuchin" },
+  { name: "cotorra" },
+  { name: "la apartada" },
+  { name: "la apartada y la frontera" },
+];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i].name);
+}
